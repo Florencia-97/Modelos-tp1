ee858fa469402e0ceb34dbea967b1eb69b4d57d0
Florencia-97 <florrr1997@gmail.com>
2021-09-26 17:49:12 -0300

    Agrega resolución trivial

--- codigo.py
+++ codigo.py
@@ -1,8 +1,9 @@
-def solucion():
+import numpy as np
+
+def parsear_documento(incompatibles, tiempo_prendas):
     n_prendas = 0
     m_incompatibilidades = 0
-    _incompatibles = {}
-    _tiempo_prendas = [0] * 20 # Arreglar
+    tiempo_prendas = [0] * 20 # Arreglar
     with open('./enunciado.txt', 'r') as archivo:
         for linea in archivo:
             parametros = linea.strip().split(' ')
@@ -14,12 +15,31 @@ def solucion():
             if tipo_de_comando == 'e':
                 n_1 = parametros[1]
                 n_2 = parametros[2]
-                _incompatibles[n_1] = _incompatibles.get(n_1, []) + [n_2]
-                _incompatibles[n_2] = _incompatibles.get(n_2, []) + [n_1]
+                incompatibles[n_1] = incompatibles.get(n_1, []) + [n_2]
+                incompatibles[n_2] = incompatibles.get(n_2, []) + [n_1]
             if tipo_de_comando == 'n':
                 n_1 = parametros[1]
                 tiempo = parametros[2]
-                _tiempo_prendas[(int(n_1) - 1)] = int(tiempo)
-    print(_tiempo_prendas)
+                tiempo_prendas[(int(n_1) - 1)] = int(tiempo)
 
-solucion()
+def buscar_solucion(incompatibles, tiempo_prendas):
+    sol = []
+    lavado_numero = 1
+    for prenda, incompatibles in incompatibles.items():
+        sol.append((prenda, lavado_numero))
+        lavado_numero += 1
+    return sol
+
+def escribir_solucion(solucion):
+    with open('./entrega_1.txt', 'w') as archivo:
+        for entrada in solucion:
+            archivo.write(f'{entrada[0]} , {entrada[1]}\n')
+
+def main():
+    incompatibles = {}
+    tiempo_prendas = []
+    parsear_documento(incompatibles, tiempo_prendas)
+    solucion = buscar_solucion(incompatibles, tiempo_prendas)
+    escribir_solucion(solucion)
+
+main()
